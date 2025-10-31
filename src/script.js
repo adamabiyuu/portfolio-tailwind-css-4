90720dd84290a89709f47b4004e5c4f160a64300
Adam Abiyu <advmabiyu@gmail.com>
2025-10-31 13:49:11 +0700

    update dark mode + fitur tambahan

--- src/script.js
+++ src/script.js
@@ -1,19 +1,68 @@
-// navbar fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+// const { doc } = require("prettier");
 
-    if( window.pageYOffset > fixedNav ) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove("navbar-fixed");
-    }
-}
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 
 // hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark mode
+const checkbox = document.querySelector("#toggle");
+const html = document.querySelector("html");
+
+checkbox.addEventListener("click", function () {
+  // checkbox.checked ? html.classList.add("dark") : html.classList.remove("dark");
+  if (checkbox.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan toggle's position sesuai mode
+const isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+// Tambahkan atau hapus class 'dark' di <html>
+document.documentElement.classList.toggle("dark", isDark);
+
+// Sesuaikan posisi toggle
+checkbox.checked = isDark;
+
+// === Saat toggle diubah ===
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
 });
